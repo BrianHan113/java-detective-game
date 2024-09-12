637cea1e9e21e5783bc9d0556d203896f1299bf6
Brian Han <shan790@aucklanduni.ac.nz>
2024-09-12 17:30:43 +1200

    Added fx ids to every interactable element in the scene

--- src/main/java/nz/ac/auckland/se206/controllers/CrimeSceneController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/CrimeSceneController.java
@@ -2,8 +2,22 @@ package nz.ac.auckland.se206.controllers;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.shape.Rectangle;
 
 public class CrimeSceneController {
 
   @FXML private Button viewEvidenceBtn;
+  @FXML private Rectangle shoeprintRect;
+  @FXML private Rectangle securityCameraRect;
+  @FXML private Rectangle hammerRect;
+  @FXML private Label timerLbl;
+  @FXML private Rectangle sonPinRect;
+  @FXML private Rectangle friendPinRect;
+  @FXML private Rectangle wifePinRect;
+
+  @FXML
+  private void showEvidence() {
+    System.out.println("Show Evidence Button Clicked");
+  }
 }

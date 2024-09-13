1b35f23b6069e3bd9f62a31e5df8f492a8fc9ff1
Kevin <kkim985@aucklanduni.ac.nz>
2024-09-13 12:56:17 +1200

    added fx:id to all key elements for each controller related to the clue scene

--- src/main/java/nz/ac/auckland/se206/controllers/CctvController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/CctvController.java
@@ -1,3 +1,16 @@
 package nz.ac.auckland.se206.controllers;
 
-public class CctvController {}
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.shape.Circle;
+
+public class CctvController {
+
+  @FXML private Circle exitCircle;
+  @FXML private Label timerLabel;
+  @FXML private Label evidenceLabel;
+  @FXML private Label fingerprintLabel;
+  @FXML private Label shoeprintLabel;
+  @FXML private Label fingerprintHideLabel;
+  @FXML private Label shoeprintHideLabel;
+}
--- src/main/java/nz/ac/auckland/se206/controllers/FootprintController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/FootprintController.java
@@ -1,3 +1,18 @@
 package nz.ac.auckland.se206.controllers;
 
-public class FootprintController {}
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.shape.Circle;
+
+public class FootprintController {
+
+  @FXML private Circle exitCircle;
+  @FXML private Label timerLabel;
+  @FXML private Label evidenceLabel;
+  @FXML private Label fingerprintLabel;
+  @FXML private Label securityCamLabel;
+  @FXML private Label fingerprintHideLabel;
+  @FXML private Label securityCamHideLabel;
+  @FXML private ImageView rulerImage;
+}

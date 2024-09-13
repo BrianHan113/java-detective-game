350db4f900167849e9ba661d942a33f01eb567d3
Brian Han <shan790@aucklanduni.ac.nz>
2024-09-13 20:05:32 +1200

    Implemented all boiler plate code for button actions

--- src/main/java/nz/ac/auckland/se206/controllers/FeedbackController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/FeedbackController.java
@@ -1,4 +1,24 @@
 package nz.ac.auckland.se206.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 
-public class FeedbackController {}
+public class FeedbackController {
+  @FXML private Label wonLostLbl;
+  @FXML private Label feedbackStatusLbl;
+  @FXML private TextArea feedbackTextArea;
+  @FXML private Button exitBtn;
+  @FXML private Button playAgainBtn;
+
+  @FXML
+  private void exitGame() {
+    System.out.println("Exit Game Button Pressed");
+  }
+
+  @FXML
+  private void resetGame() {
+    System.out.println("Reset Game Button Pressed");
+  }
+}

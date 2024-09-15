7ee8688f0c46973d9ba10967febc5aef23ed5e26
Brian Han <shan790@aucklanduni.ac.nz>
2024-09-15 16:30:52 +1200

    Implemented map navigation for all suspects and the crime scene

--- src/main/java/nz/ac/auckland/se206/controllers/ExwifeController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/ExwifeController.java
@@ -1,16 +1,20 @@
 package nz.ac.auckland.se206.controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.shape.Rectangle;
 import nz.ac.auckland.apiproxy.exceptions.ApiProxyException;
 import nz.ac.auckland.se206.Chat;
+import nz.ac.auckland.se206.SceneManager;
+import nz.ac.auckland.se206.SceneManager.AppUi;
 
-public class ExwifeController extends Chat{
+public class ExwifeController extends Chat {
 
   @FXML private Button sendButton;
   @FXML private TextArea txtArea;
@@ -27,13 +31,36 @@ public class ExwifeController extends Chat{
    */
   @FXML
   public void initialize() throws ApiProxyException {
-    System.out.println("Initialized Ex-wife Chat: "+this);
+    System.out.println("Initialized Ex-wife Chat: " + this);
     // Bind <Enter> key to sendButton
-    txtInput.setOnKeyPressed(event -> {
-      if (event.getCode() == KeyCode.ENTER) {
-        sendButton.fire();
-      }
-    });
+    txtInput.setOnKeyPressed(
+        event -> {
+          if (event.getCode() == KeyCode.ENTER) {
+            sendButton.fire();
+          }
+        });
     setRole("Ex-Wife", "chat.txt");
+  }
+
+  @FXML
+  private void handleNavigationClick(MouseEvent event) {
+    Rectangle shape = (Rectangle) event.getSource();
+    String shapeId = shape.getId();
+    Scene sceneOfShape = shape.getScene();
+
+    switch (shapeId) {
+      case "friendPinRect":
+        sceneOfShape.setRoot(SceneManager.getUiRoot(AppUi.FRIEND));
+        break;
+      case "sonPinRect":
+        sceneOfShape.setRoot(SceneManager.getUiRoot(AppUi.SON));
+        break;
+      case "crimeScenePinRect":
+        sceneOfShape.setRoot(SceneManager.getUiRoot(AppUi.CRIME_SCENE));
+        break;
+
+      default:
+        break;
+    }
   }
 }

544e0764271454b2aa9b8070821338f834d4965d
Brian Han <shan790@aucklanduni.ac.nz>
2024-09-16 17:22:11 +1200

    Added fingerprint brush dragging mechanic, need to rework dusting

--- src/main/java/nz/ac/auckland/se206/controllers/HammerController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/HammerController.java
@@ -21,14 +21,29 @@ public class HammerController {
   }
 
   @FXML
-  private void handleMouseDragged(MouseEvent event) {
-    if (!isFingerprintDusted) {
-      this.opacity = this.opacity + 0.001;
-      fingerprintImage.setOpacity(opacity);
-      System.out.println("weiofj");
-      if (opacity >= 1) {
-        isFingerprintDusted = true;
-      }
-    }
+  private void handleBrushDrag(MouseEvent event) {
+    moveBrush(event.getSceneX(), event.getSceneY());
   }
+
+  private void moveBrush(double x, double y) {
+    // Prevent brush from going off screen
+    if (x < 50 || x > 850 || y < 50 || y > 500) {
+      return;
+    }
+
+    brushImage.setLayoutX(x - brushImage.getFitWidth() / 2);
+    brushImage.setLayoutY(y - brushImage.getFitHeight() / 2);
+  }
+
+  // @FXML
+  // private void handleMouseDragged(MouseEvent event) {
+  //   if (!isFingerprintDusted) {
+  //     this.opacity = this.opacity + 0.001;
+  //     fingerprintImage.setOpacity(opacity);
+  //     System.out.println("weiofj");
+  //     if (opacity >= 1) {
+  //       isFingerprintDusted = true;
+  //     }
+  //   }
+  // }
 }

5707ae0e39f5d815f3590f70ca16a79bf914f5be
Kevin <kkim985@aucklanduni.ac.nz>
2024-09-16 16:25:48 +1200

    implemented the timer to all the other scenes (except for the guessing room)

--- src/main/java/nz/ac/auckland/se206/controllers/FingerprintController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/FingerprintController.java
@@ -1,9 +1,13 @@
 package nz.ac.auckland.se206.controllers;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.shape.Circle;
+import javafx.util.Duration;
+import nz.ac.auckland.se206.TimeManager;
 
 public class FingerprintController {
 
@@ -16,4 +20,32 @@ public class FingerprintController {
   @FXML private Label shoeprintHideLabel;
   @FXML private Label securityCamHideLabel;
   @FXML private Label imageHideLabel;
+
+  private int minute;
+  private int second;
+  private int millisecond;
+  private Timeline timeline;
+  private TimeManager timeManager = TimeManager.getInstance();
+
+  public void initialize() {
+    timerLabel.setText(timeManager.formatTime());
+    decrementTime();
+  }
+
+  private void decrementTime() {
+    timeline = new Timeline(new KeyFrame(Duration.millis(1), e -> updateTimerLabel()));
+    timeline.setCycleCount(Timeline.INDEFINITE);
+    timeline.play();
+  }
+
+  private void updateTimerLabel() {
+    minute = timeManager.getMinute();
+    second = timeManager.getSecond();
+    millisecond = timeManager.getMillisecond();
+    if (minute == 0 && second == 0 && millisecond == 0) {
+      timerLabel.setText("Time's Up!");
+    } else {
+      timerLabel.setText(String.format("%02d:%02d:%03d", minute, second, millisecond));
+    }
+  }
 }

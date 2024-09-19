b997ad70a569f27c5975093a3f359d90e7073e7c
Brian Han <shan790@aucklanduni.ac.nz>
2024-09-19 14:24:23 +1200

    Refactored code to use the AudioPlayerManager instead of making its own media player instance which also fixed overlapping audio

--- src/main/java/nz/ac/auckland/se206/Voicelines.java
+++ src/main/java/nz/ac/auckland/se206/Voicelines.java
@@ -1,70 +1,50 @@
 package nz.ac.auckland.se206;
 
 import java.util.Random;
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
 
 public class Voicelines {
-  private static MediaPlayer mediaPlayer;
+  private static AudioPlayerManager audioPlayer = AudioPlayerManager.getInstance();
 
   public static void fillerVoiceLines(String role) {
     // Generate random int from 1 to 4 to get random voice lines
     Random random = new Random();
     int randomInt = random.nextInt(4) + 1;
-    Media sound;
 
     // Play a random voice line for each suspect in the game
     switch (role) {
       case "Ex-Wife":
-        sound =
-            new Media(
-                App.class
-                    .getResource("/sounds/exwife_tts/line_" + randomInt + ".mp3")
-                    .toExternalForm());
+        audioPlayer.playAudio("/exwife_tts/line_" + randomInt + ".mp3");
         break;
       case "Son":
-        sound =
-            new Media(
-                App.class
-                    .getResource("/sounds/son_tts/line_" + randomInt + ".mp3")
-                    .toExternalForm());
+        audioPlayer.playAudio("/son_tts/line_" + randomInt + ".mp3");
         break;
       case "Friend":
-        sound =
-            new Media(
-                App.class
-                    .getResource("/sounds/friend_tts/line_" + randomInt + ".mp3")
-                    .toExternalForm());
+        audioPlayer.playAudio("/friend_tts/line_" + randomInt + ".mp3");
         break;
 
       default:
-        sound = new Media(App.class.getResource("/sounds/friend_tts/line_1.mp3").toExternalForm());
+        audioPlayer.playAudio("/friend_tts/line_" + randomInt + ".mp3");
         break;
     }
-    mediaPlayer = new MediaPlayer(sound);
-    mediaPlayer.play();
   }
 
   public static void introVoiceLines(String role) {
-    Media sound;
 
     // Play a intro voice line for each suspect in the game
     switch (role) {
       case "Ex-Wife":
-        sound = new Media(App.class.getResource("/sounds/exwife_tts/intro.mp3").toExternalForm());
+        audioPlayer.playAudio("/exwife_tts/intro.mp3");
         break;
       case "Son":
-        sound = new Media(App.class.getResource("/sounds/son_tts/intro.mp3").toExternalForm());
+        audioPlayer.playAudio("/son_tts/intro.mp3");
         break;
       case "Friend":
-        sound = new Media(App.class.getResource("/sounds/friend_tts/intro.mp3").toExternalForm());
+        audioPlayer.playAudio("/friend_tts/intro.mp3");
         break;
 
       default:
-        sound = new Media(App.class.getResource("/sounds/friend_tts/line_1.mp3").toExternalForm());
+        audioPlayer.playAudio("/friend_tts/intro.mp3");
         break;
     }
-    mediaPlayer = new MediaPlayer(sound);
-    mediaPlayer.play();
   }
 }
